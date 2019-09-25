2a39e8d1751e5c1bffe56de8e2e1f07b19e576a1
kazuma uehara <e175740@ie.u-ryukyu.ac.jp>
2019-09-25 13:03:18 +0900

    Initial Commit

---
+++ enpit_test2_pg/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  enpit_test2_pg
+//
+//  Created by uehara kazuma on 2019/09/25.
+//  Copyright © 2019 uehara kazuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var alertController: UIAlertController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+    
+    
+    
+    func alert(title:String, message:String) {
+        alertController = UIAlertController(title: title,
+                                   message: message,
+                                   preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK",
+                                       style: .default,
+                                       handler: nil))
+        present(alertController, animated: true)
+    }
+    
+    
+    @IBAction func button(_ sender: UIButton) {
+        alert(title: "Alert!!!",
+        message: "その本は購入済みです！")
+    }
+    
+    
+}
+
